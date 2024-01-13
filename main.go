f5dfb2a44c31a41365cd0a70e36bcdc2f8452a9b
cassius <cassiusbessa@gmail.com>
2024-01-13 19:54:21 -0300

    fix: :bug: fix create db

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	postgres "github.com/cassiusbessa/satback/db"
 	controllers "github.com/cassiusbessa/satback/http"
 )
 
 func main() {
-	postgres.CreateDb()
+	for i := 0; i < 30; i++ {
+		err := postgres.CreateDb()
+		if err != nil {
+			fmt.Printf("Erro ao conectar ao banco de dados: %v\n", err)
+			time.Sleep(time.Second * 2)
+		} else {
+			break
+		}
+	}
 	controllers.RegisterHandlers()
 	http.ListenAndServe(":8080", nil)
 }
